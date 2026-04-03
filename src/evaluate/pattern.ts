e793539bddc43d86074bd3554b774e9457ae01e5
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 19:15:33 -0500

    style: js-beautify formatting (src/evaluate/pattern.ts)

--- src/evaluate/pattern.ts
+++ src/evaluate/pattern.ts
@@ -1,21 +1,34 @@
-import { NOINIT, DEADZONE } from '../share/const'
-import { VarKind } from '../scope/variable'
-import { Identifier } from './identifier'
-import { assign } from '../share/util'
-import { pattern } from './helper'
+import {
+  NOINIT,
+  DEADZONE
+} from '../share/const'
+import {
+  VarKind
+} from '../scope/variable'
+import {
+  Identifier
+} from './identifier'
+import {
+  assign
+} from '../share/util'
+import {
+  pattern
+} from './helper'
 import * as acorn from 'acorn'
 import Scope from '../scope'
 import evaluate from '.'
 
 export interface PatternOptions {
-  kind?: VarKind
-  hoist?: boolean
-  onlyBlock?: boolean
-  feed?: any
+  kind ? : VarKind
+  hoist ? : boolean
+  onlyBlock ? : boolean
+  feed ? : any
 }
 
 export function* ObjectPattern(node: acorn.ObjectPattern, scope: Scope, options: PatternOptions = {}) {
-  const { kind = 'var', hoist = false, onlyBlock = false, feed = {} } = options
+  const {
+    kind = 'var', hoist = false, onlyBlock = false, feed = {}
+  } = options
   const fedKeys: string[] = []
   for (let i = 0; i < node.properties.length; i++) {
     const property = node.properties[i]
@@ -26,10 +39,18 @@ export function* ObjectPattern(node: acorn.ObjectPattern, scope: Scope, options:
           if (value.type === 'Identifier') {
             scope[kind](value.name, onlyBlock ? DEADZONE : kind === 'var' ? NOINIT : undefined)
           } else {
-            yield* pattern(value, scope, { kind, hoist, onlyBlock })
+            yield* pattern(value, scope, {
+              kind,
+              hoist,
+              onlyBlock
+            })
           }
         } else {
-          yield* RestElement(property, scope, { kind, hoist, onlyBlock })
+          yield* RestElement(property, scope, {
+            kind,
+            hoist,
+            onlyBlock
+          })
         }
       }
     } else if (property.type === 'Property') {
@@ -40,23 +61,34 @@ export function* ObjectPattern(node: acorn.ObjectPattern, scope: Scope, options:
         key = (property.key as acorn.Identifier).name
       }
       fedKeys.push(key)
-      
+
       const value = property.value
       if (value.type === 'Identifier') {
         scope[kind](value.name, feed[key])
       } else {
-        yield* pattern(value, scope, { kind, feed: feed[key] })
+        yield* pattern(value, scope, {
+          kind,
+          feed: feed[key]
+        })
       }
     } else {
       const rest = assign({}, feed)
       for (let i = 0; i < fedKeys.length; i++) delete rest[fedKeys[i]]
-      yield* RestElement(property, scope, { kind, feed: rest })
+      yield* RestElement(property, scope, {
+        kind,
+        feed: rest
+      })
     }
   }
 }
 
 export function* ArrayPattern(node: acorn.ArrayPattern, scope: Scope, options: PatternOptions = {}) {
-  const { kind, hoist = false, onlyBlock = false, feed = [] } = options
+  const {
+    kind,
+    hoist = false,
+    onlyBlock = false,
+    feed = []
+  } = options
   const result = []
   for (let i = 0; i < node.elements.length; i++) {
     const element = node.elements[i]
@@ -66,7 +98,11 @@ export function* ArrayPattern(node: acorn.ArrayPattern, scope: Scope, options: P
         if (element.type === 'Identifier') {
           scope[kind](element.name, onlyBlock ? DEADZONE : kind === 'var' ? NOINIT : undefined)
         } else {
-          yield* pattern(element, scope, { kind, hoist, onlyBlock })
+          yield* pattern(element, scope, {
+            kind,
+            hoist,
+            onlyBlock
+          })
         }
       }
     } else if (element.type === 'Identifier') {
@@ -75,14 +111,22 @@ export function* ArrayPattern(node: acorn.ArrayPattern, scope: Scope, options: P
         scope[kind](element.name, feed[i])
       } else {
         // If kind is undefined, it's a statement
-        const variable = yield* Identifier(element, scope, { getVar: true })
+        const variable = yield* Identifier(element, scope, {
+          getVar: true
+        })
         variable.set(feed[i])
         result.push(variable.get())
       }
     } else if (element.type === 'RestElement') {
-      yield* RestElement(element, scope, { kind, feed: feed.slice(i) })
+      yield* RestElement(element, scope, {
+        kind,
+        feed: feed.slice(i)
+      })
     } else {
-      yield* pattern(element, scope, { kind, feed: feed[i] })
+      yield* pattern(element, scope, {
+        kind,
+        feed: feed[i]
+      })
     }
   }
   if (result.length) {
@@ -91,14 +135,23 @@ export function* ArrayPattern(node: acorn.ArrayPattern, scope: Scope, options: P
 }
 
 export function* RestElement(node: acorn.RestElement, scope: Scope, options: PatternOptions = {}) {
-  const { kind, hoist = false, onlyBlock = false, feed = [] } = options
+  const {
+    kind,
+    hoist = false,
+    onlyBlock = false,
+    feed = []
+  } = options
   const arg = node.argument
   if (hoist) {
     if (onlyBlock || kind === 'var') {
       if (arg.type === 'Identifier') {
         scope[kind](arg.name, onlyBlock ? DEADZONE : kind === 'var' ? NOINIT : undefined)
       } else {
-        yield* pattern(arg, scope, { kind, hoist, onlyBlock })
+        yield* pattern(arg, scope, {
+          kind,
+          hoist,
+          onlyBlock
+        })
       }
     }
   } else if (arg.type === 'Identifier') {
@@ -107,28 +160,42 @@ export function* RestElement(node: acorn.RestElement, scope: Scope, options: Pat
       scope[kind](arg.name, feed)
     } else {
       // If kind is undefined, it's a statement
-      const variable = yield* Identifier(arg, scope, { getVar: true })
+      const variable = yield* Identifier(arg, scope, {
+        getVar: true
+      })
       variable.set(feed)
     }
   } else {
-    yield* pattern(arg, scope, { kind, feed })
+    yield* pattern(arg, scope, {
+      kind,
+      feed
+    })
   }
 }
 
 export function* AssignmentPattern(node: acorn.AssignmentPattern, scope: Scope, options: PatternOptions = {}) {
-  const { kind = 'var', hoist = false, onlyBlock = false, feed = yield* evaluate(node.right, scope) } = options
+  const {
+    kind = 'var', hoist = false, onlyBlock = false, feed = yield* evaluate(node.right, scope)
+  } = options
   const left = node.left
   if (hoist) {
     if (onlyBlock || kind === 'var') {
       if (left.type === 'Identifier') {
         scope[kind](left.name, onlyBlock ? DEADZONE : kind === 'var' ? NOINIT : undefined)
       } else {
-        yield* pattern(left, scope, { kind, hoist, onlyBlock })
+        yield* pattern(left, scope, {
+          kind,
+          hoist,
+          onlyBlock
+        })
       }
     }
   } else if (left.type === 'Identifier') {
     scope[kind](left.name, feed)
   } else {
-    yield* pattern(left, scope, { kind, feed })
+    yield* pattern(left, scope, {
+      kind,
+      feed
+    })
   }
 }
