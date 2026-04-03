6fb31288addf111f218a82d2772153668062bff0
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 19:15:50 -0500

    style: js-beautify formatting (src/share/util.ts)

--- src/share/util.ts
+++ src/share/util.ts
@@ -22,13 +22,14 @@ export function getProto(obj: any) {
 }
 
 const getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor
+
 function getGetterOrSetter(method: 'get' | 'set', obj: any, key: string) {
   while (obj) {
     const descriptor = getOwnPropertyDescriptor(obj, key)
-    const value = typeof descriptor !== 'undefined'
-      && typeof descriptor.writable === 'undefined'
-      && typeof descriptor[method] === 'function'
-      && descriptor[method]
+    const value = typeof descriptor !== 'undefined' &&
+      typeof descriptor.writable === 'undefined' &&
+      typeof descriptor[method] === 'function' &&
+      descriptor[method]
     if (value) {
       return value
     } else {
@@ -58,17 +59,17 @@ export function inherits(
 }
 
 export function _assign(target: any): any {
-    for (let i = 1; i < arguments.length; ++i) {
-      const source = arguments[i]
-      for (const key in source) {
-        if (hasOwn(source, key)) {
-          target[key] = source[key]
-        }
+  for (let i = 1; i < arguments.length; ++i) {
+    const source = arguments[i]
+    for (const key in source) {
+      if (hasOwn(source, key)) {
+        target[key] = source[key]
       }
     }
-    return target
+  }
+  return target
 }
-export const assign = Object.assign ||  _assign
+export const assign = Object.assign || _assign
 
 declare let WebAssembly: any // Avoid typescript error
 let names: string[] = []
@@ -85,82 +86,269 @@ try {
     names = getOwnNames(globalObj = global).filter(n => n !== 'GLOBAL' && n !== 'root')
   } catch (err) {
     // Unknow environment, simulate a global environment
-    try { globalObj.Object = Object                         } catch (err) { /* empty */ }
-    try { globalObj.Function = Function                     } catch (err) { /* empty */ }
-    try { globalObj.Array = Array                           } catch (err) { /* empty */ }
-    try { globalObj.Number = Number                         } catch (err) { /* empty */ }
-    try { globalObj.parseFloat = parseFloat                 } catch (err) { /* empty */ }
-    try { globalObj.parseInt = parseInt                     } catch (err) { /* empty */ }
-    try { globalObj.Infinity = Infinity                     } catch (err) { /* empty */ }
-    try { globalObj.NaN = NaN                               } catch (err) { /* empty */ }
-    try { globalObj.undefined = undefined                   } catch (err) { /* empty */ }
-    try { globalObj.Boolean = Boolean                       } catch (err) { /* empty */ }
-    try { globalObj.String = String                         } catch (err) { /* empty */ }
-    try { globalObj.Symbol = Symbol                         } catch (err) { /* empty */ }
-    try { globalObj.Date = Date                             } catch (err) { /* empty */ }
-    try { globalObj.Promise = Promise                       } catch (err) { /* empty */ }
-    try { globalObj.RegExp = RegExp                         } catch (err) { /* empty */ }
-    try { globalObj.Error = Error                           } catch (err) { /* empty */ }
-    try { globalObj.EvalError = EvalError                   } catch (err) { /* empty */ }
-    try { globalObj.RangeError = RangeError                 } catch (err) { /* empty */ }
-    try { globalObj.ReferenceError = ReferenceError         } catch (err) { /* empty */ }
-    try { globalObj.SyntaxError = SyntaxError               } catch (err) { /* empty */ }
-    try { globalObj.TypeError = TypeError                   } catch (err) { /* empty */ }
-    try { globalObj.URIError = URIError                     } catch (err) { /* empty */ }
-    try { globalObj.JSON = JSON                             } catch (err) { /* empty */ }
-    try { globalObj.Math = Math                             } catch (err) { /* empty */ }
-    try { globalObj.console = console                       } catch (err) { /* empty */ }
-    try { globalObj.Intl = Intl                             } catch (err) { /* empty */ }
-    try { globalObj.ArrayBuffer = ArrayBuffer               } catch (err) { /* empty */ }
-    try { globalObj.Uint8Array = Uint8Array                 } catch (err) { /* empty */ }
-    try { globalObj.Int8Array = Int8Array                   } catch (err) { /* empty */ }
-    try { globalObj.Uint16Array = Uint16Array               } catch (err) { /* empty */ }
-    try { globalObj.Int16Array = Int16Array                 } catch (err) { /* empty */ }
-    try { globalObj.Uint32Array = Uint32Array               } catch (err) { /* empty */ }
-    try { globalObj.Int32Array = Int32Array                 } catch (err) { /* empty */ }
-    try { globalObj.Float32Array = Float32Array             } catch (err) { /* empty */ }
-    try { globalObj.Float64Array = Float64Array             } catch (err) { /* empty */ }
-    try { globalObj.Uint8ClampedArray = Uint8ClampedArray   } catch (err) { /* empty */ }
-    try { globalObj.DataView = DataView                     } catch (err) { /* empty */ }
-    try { globalObj.Map = Map                               } catch (err) { /* empty */ }
-    try { globalObj.Set = Set                               } catch (err) { /* empty */ }
-    try { globalObj.WeakMap = WeakMap                       } catch (err) { /* empty */ }
-    try { globalObj.WeakSet = WeakSet                       } catch (err) { /* empty */ }
-    try { globalObj.Proxy = Proxy                           } catch (err) { /* empty */ }
-    try { globalObj.Reflect = Reflect                       } catch (err) { /* empty */ }
-    try { globalObj.BigInt = BigInt                         } catch (err) { /* empty */ }
-    try { globalObj.decodeURI = decodeURI                   } catch (err) { /* empty */ }
-    try { globalObj.decodeURIComponent = decodeURIComponent } catch (err) { /* empty */ }
-    try { globalObj.encodeURI = encodeURI                   } catch (err) { /* empty */ }
-    try { globalObj.encodeURIComponent = encodeURIComponent } catch (err) { /* empty */ }
-    try { globalObj.escape = escape                         } catch (err) { /* empty */ }
-    try { globalObj.unescape = unescape                     } catch (err) { /* empty */ }
-    try { globalObj.eval = eval                             } catch (err) { /* empty */ }
-    try { globalObj.isFinite = isFinite                     } catch (err) { /* empty */ }
-    try { globalObj.isNaN = isNaN                           } catch (err) { /* empty */ }
-    try { globalObj.SharedArrayBuffer = SharedArrayBuffer   } catch (err) { /* empty */ }
-    try { globalObj.Atomics = Atomics                       } catch (err) { /* empty */ }
-    try { globalObj.WebAssembly = WebAssembly               } catch (err) { /* empty */ }
-    try { globalObj.clearInterval = clearInterval           } catch (err) { /* empty */ }
-    try { globalObj.clearTimeout = clearTimeout             } catch (err) { /* empty */ }
-    try { globalObj.setInterval = setInterval               } catch (err) { /* empty */ }
-    try { globalObj.setTimeout = setTimeout                 } catch (err) { /* empty */ }
-    try { globalObj.crypto = crypto                         } catch (err) { /* empty */ }
+    try {
+      globalObj.Object = Object
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Function = Function
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Array = Array
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Number = Number
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.parseFloat = parseFloat
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.parseInt = parseInt
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Infinity = Infinity
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.NaN = NaN
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.undefined = undefined
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Boolean = Boolean
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.String = String
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Symbol = Symbol
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Date = Date
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Promise = Promise
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.RegExp = RegExp
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Error = Error
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.EvalError = EvalError
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.RangeError = RangeError
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.ReferenceError = ReferenceError
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.SyntaxError = SyntaxError
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.TypeError = TypeError
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.URIError = URIError
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.JSON = JSON
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Math = Math
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.console = console
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Intl = Intl
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.ArrayBuffer = ArrayBuffer
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Uint8Array = Uint8Array
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Int8Array = Int8Array
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Uint16Array = Uint16Array
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Int16Array = Int16Array
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Uint32Array = Uint32Array
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Int32Array = Int32Array
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Float32Array = Float32Array
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Float64Array = Float64Array
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Uint8ClampedArray = Uint8ClampedArray
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.DataView = DataView
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Map = Map
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Set = Set
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.WeakMap = WeakMap
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.WeakSet = WeakSet
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Proxy = Proxy
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Reflect = Reflect
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.BigInt = BigInt
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.decodeURI = decodeURI
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.decodeURIComponent = decodeURIComponent
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.encodeURI = encodeURI
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.encodeURIComponent = encodeURIComponent
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.escape = escape
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.unescape = unescape
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.eval = eval
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.isFinite = isFinite
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.isNaN = isNaN
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.SharedArrayBuffer = SharedArrayBuffer
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.Atomics = Atomics
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.WebAssembly = WebAssembly
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.clearInterval = clearInterval
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.clearTimeout = clearTimeout
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.setInterval = setInterval
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.setTimeout = setTimeout
+    } catch (err) {
+      /* empty */ }
+    try {
+      globalObj.crypto = crypto
+    } catch (err) {
+      /* empty */ }
     names = getOwnNames(globalObj)
   }
 }
 if (globalObj.Symbol) {
-  !globalObj.Symbol.iterator && (globalObj.Symbol.iterator = createSymbol('iterator'))
-  !globalObj.Symbol.asyncIterator && (globalObj.Symbol.asyncIterator = createSymbol('asynciterator'))
+  !globalObj.Symbol.iterator && (globalObj.Symbol.iterator = createSymbol('iterator')) !globalObj.Symbol.asyncIterator && (globalObj.Symbol.asyncIterator = createSymbol('asynciterator'))
 }
 const win = create({})
 for (let i = 0; i < names.length; i++) {
   const name = names[i]
-  try { win[name] = globalObj[name] } catch (err) { /* empty */ }
+  try {
+    win[name] = globalObj[name]
+  } catch (err) {
+    /* empty */ }
 }
 export const WINDOW = createSymbol('window')
 export function createSandBox() {
-  return assign(create({ [WINDOW]: globalObj }), win)
+  return assign(create({
+    [WINDOW]: globalObj
+  }), win)
 }
 
 export function createSymbol(key: string) {
@@ -170,8 +358,7 @@ export function createSymbol(key: string) {
 export function getAsyncIterator(obj: any) {
   let iterator: any
   if (typeof Symbol === 'function') {
-    iterator = obj[Symbol.asyncIterator]
-    !iterator && (iterator = obj[Symbol.iterator])
+    iterator = obj[Symbol.asyncIterator] !iterator && (iterator = obj[Symbol.iterator])
   }
   if (iterator) {
     return iterator.call(obj)
@@ -182,7 +369,10 @@ export function getAsyncIterator(obj: any) {
     return {
       next() {
         if (obj && i >= obj.length) obj = undefined
-        return { value: obj && obj[i++], done: !obj }
+        return {
+          value: obj && obj[i++],
+          done: !obj
+        }
       }
     }
   }
