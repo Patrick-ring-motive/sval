9990356524edc673d781e84c782208641f7d51d8
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 19:16:18 -0500

    style: js-beautify formatting (types/index.d.ts)

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,20 +1,23 @@
-import { ecmaVersion, Node } from 'acorn';
+import {
+  ecmaVersion,
+  Node
+} from 'acorn';
 export interface SvalOptions {
-    ecmaVer?: ecmaVersion;
-    sandBox?: boolean;
+  ecmaVer ? : ecmaVersion;
+  sandBox ? : boolean;
 }
 declare class Sval {
-    static version: string;
-    private options;
-    private scope;
-    exports: {
-        [name: string]: any;
-    };
-    constructor(options?: SvalOptions);
-    import(nameOrModules: string | {
-        [name: string]: any;
-    }, mod?: any): void;
-    parse(code: string, parser?: (code: string, options: SvalOptions) => Node): Node;
-    run(code: string | Node): void;
+  static version: string;
+  private options;
+  private scope;
+  exports: {
+    [name: string]: any;
+  };
+  constructor(options ? : SvalOptions);
+  import(nameOrModules: string | {
+    [name: string]: any;
+  }, mod ? : any): void;
+  parse(code: string, parser ? : (code: string, options: SvalOptions) => Node): Node;
+  run(code: string | Node): void;
 }
 export default Sval;
